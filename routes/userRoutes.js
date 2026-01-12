16db29c39e5a1ec42d40201bce3deb3b85c4914e
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-01-12 07:41:55 +0100

    feat: rutas de userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,3 +5,12 @@ export const router = Router();
 // Ejemplo import middelwares: import {esMayor, otroMiddleware} from "../middlewares/userMiddlewares.js";
 
 //router.get('/',[/*middlewares */],/*controlador.getUsers*/);
+router.post('/login', controlador.login);
+router.post('/register', controlador.register); //-> create
+
+//CRUD, TO DO: MIDDLEWARES
+router.get('/', controlador.getUsers);
+router.get('/:id', controlador.getUser);
+router.put('/:id', controlador.updateUser);
+router.delete('/:id', controlador.deleteUser);
+
