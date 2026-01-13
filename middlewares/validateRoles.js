0fc7aa81853b1ca4a796e92bf1dccec6f125593e
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-01-13 19:20:37 +0100

    feat: middlewares
    se han implementado:
    - verificador de token jwt
    - admin
    -standard
    -hasRole (para comprobar si al menos tiene uno de los roles)

--- middlewares/validateRoles.js
+++ middlewares/validateRoles.js
@@ -1,5 +1,14 @@
 export const isAdmin = (req, res, next) =>{
-    if(!req.roles.includes('admin')){
+   
+    if(!req.roles){
+        return res.status(500).json({
+            'Error': 'Se ha intentado validar el rol sin validar el token primero'
+        });
+    }
+   
+    const haveRolAdmin = req.roles.some(rol => rol.name === 'admin');
+
+    if(!haveRolAdmin){
         return res.status(401).json({
             'msg': 'No es administrador'
         });
@@ -9,11 +18,42 @@ export const isAdmin = (req, res, next) =>{
 }
 
 export const isStandard = (req, res, next) =>{
-    if(!req.roles.includes('standard')){
+
+    if(!req.roles){
+        return res.status(500).json({
+            'Error': 'Se ha intentado validar el rol sin validar el token primero'
+        });
+    }
+
+     const haveRolStandard = req.roles.some(rol => rol.name === 'standard');
+
+    if(!haveRolStandard){
         return res.status(401).json({
             'msg': 'No es standard'
         });
     }
     console.log(req.email + "accediendo como standard...");
     next();
+}
+
+
+//por lo visto para poder hacer una cosa que quiero tengo que implementar esto
+
+export const hasRole = (...allowedRoles) =>{
+    return (req, res, next) =>{
+        if(!req.roles){
+            return res.status(500).json({
+                msg:'Se requiere verificar el jwt antes que el rol'
+            });
+        }
+
+        const isAuthorized = req.roles.some(userRole => allowedRoles.includes(userRole.name));
+
+        if(!isAuthorized){
+            return res.status(401).json({
+                msg: `El servicio requiere uno de estos roles: ${allowedRoles.join(', ')}`
+            });
+        }
+        next();
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import controlador from '../controllers/userController.js';
 export const router = Router();
-import { isAdmin, isStandard } from "../middlewares/validateRoles.js";
+import { validateJWT } from "../middlewares/ValidateJWT.js";
+import { hasRole } from "../middlewares/validateRoles.js";
+
+// import { isAdmin, isStandard } from "../middlewares/validateRoles.js";
 // Ejemplo import middelwares: import {esMayor, otroMiddleware} from "../middlewares/userMiddlewares.js";
 
 //router.get('/',[/*middlewares */],/*controlador.getUsers*/);
@@ -9,8 +12,8 @@ router.post('/login', controlador.login);
 router.post('/register', controlador.register); //-> create
 
 
-router.get('/',isAdmin,  controlador.getUsers);
-router.get('/:id', [isAdmin, isStandard], controlador.getUser);
-router.put('/:id',[isAdmin, isStandard], controlador.updateUser);
-router.delete('/:id', isAdmin, controlador.deleteUser);
+router.get('/',[validateJWT,hasRole('admin')],  controlador.getUsers);
+router.get('/:id', [validateJWT,hasRole('admin', 'standard')], controlador.getUser);
+router.put('/:id',[validateJWT,hasRole('admin', 'standard')], controlador.updateUser);
+router.delete('/:id', [validateJWT,hasRole('admin')], controlador.deleteUser);
 
